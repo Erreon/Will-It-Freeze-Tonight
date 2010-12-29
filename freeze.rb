75d4d8dd26fc6ae72920abf7d212a33e12675b8d
Jason Carpentier <jcarpentier@gmail.com>
2010-12-29 15:50:31 -0600

    Working on readme and added MIT license

--- freeze.rb
+++ freeze.rb
@@ -3,6 +3,7 @@ require 'sinatra/flash'
 require 'google_weather'
 require 'erb'
 
+
 get '/' do
   @title = "WillItFreezeTonight.com"
   erb :index
@@ -11,29 +12,30 @@ end
 enable :sessions
 
 post '/weather' do
-    begin
-      place = params[:place]
-      weather = GoogleWeather.new(place)
-      forecast = weather.forecast_conditions[0]
-      low = forecast.low.to_i
-      if low <= 32
-        @answer = "<div class='answer'>Yes</div><div class='today'>today's low is</div><div class='low'>#{low}&#176;F</div>"
-      else
-        @answer = "<div class='answer'>No</div><div class='today'>today's low is</div><div class='low'>#{low}&#176;F</div>"
-      end
-      
+  begin
+    place = params[:place]
+    weather = GoogleWeather.new(place)
+    today = weather.forecast_conditions[0]
+    low = today.low.to_i
+  
+    if low <= 32
+      @answer = "<div class='answer'>Yes</div><div class='today'>today's low is</div><div class='low'>#{low}&#176;F</div>"
+    else
+      @answer = "<div class='answer'>No</div><div class='today'>today's low is</div><div class='low'>#{low}&#176;F</div>"
+    end  
+  
     rescue NoMethodError
-        if place.downcase == "hell"
-          @answer = "Probably not, but if the Cowboys won today... Yes and there is snow too!"
-          erb :weather
-        else
-          flash[:warning] = "Either this place doesn't exist or we don't have access to weather info there.  Try searching for a nearby town or zipcode."
-          redirect '/'
-        end
-    end
+      if place.downcase == "hell"
+        @answer = "Probably not, but if the Cowboys won today... Yes and there is snow too!"
+        erb :weather
+      else
+        flash[:warning] = "Either this place doesn't exist or we don't have access to weather info there.  Try searching for a nearby town or zipcode."
+        redirect '/'
+      end
+  end
   erb :weather
 end
 
-get '/weather' do
- redirect '/'
+not_found do
+  redirect '/'
 end
